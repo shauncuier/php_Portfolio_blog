a653a792c220d52d9492e41934751941f7469866
Shaun <shauncuier@gmail.com>
2021-05-31 03:55:40 +0600

    Tailwind CSS setup

--- admin/include/header.php
+++ admin/include/header.php
@@ -5,11 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome To Admin Panel</title>
-    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <!-- <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" referrerpolicy="no-referrer" />
+    
+    <link rel="stylesheet" href="css/tailwind.css">
+
 </head>
 <body class="">
 
-        <header class="bg-gray-800 py-6">
+        <!-- <header class="bg-gray-800 py-6">
             <div class="container mx-auto flex justify-between items-center px-6 w-4/5">
                 <div>
                     <a href="index.php" class="text-lg font-semibold text-gray-100 no-underline">
@@ -17,10 +21,11 @@
                     </a>
                 </div>
                 <nav class="space-x-4 text-gray-300 text-sm sm:text-base">
-                    <a class="no-underline hover:underline" href="index.php">Admin Home</a>
-                    <a class="no-underline hover:underline" href="blog.index.php">Blog</a>
-                    <a class="no-underline hover:underline" href="portfolio.index.php">Portfolio</a>
-                    <a class="no-underline hover:underline" href="login.php">Login/Singup</a>
+                    <a class="no-underline hover:underline" href="index.php"><i class="fas fa-user-shield"></i> Admin Home</a>
+                    <a class="no-underline hover:underline" href="blog.index.php"><i class="fas fa-atlas"></i> Blog</a>
+                    <a class="no-underline hover:underline" href="portfolio.index.php"><i class="fas fa-briefcase"></i> Portfolio</a>
+                    <a class="no-underline hover:underline" href="login.php"><i class="fas fa-user-plus"></i> Login/Singup</a>
                 </nav>
             </div>
-        </header>
+        </header> -->
+        
